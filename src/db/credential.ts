d1b4a44882e80204d233ab52f8e06b6bf70be501
kefer <morafi.1999.16@gmail.com>
2022-12-05 23:03:31 -0500

    se cambia el nombre de las variabÃles de entorno por las de railway

--- src/db/credential.ts
+++ src/db/credential.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 
 export class Credential {
-	readonly host: string = process.env.API_DB_HOST || "";
-	readonly database: string = process.env.API_DB_DATABASE || "";
-	readonly port: number = parseInt(process.env.API_DB_PORT || "0");
-	readonly user: string = process.env.API_DB_USER || "";
-	readonly password: string = process.env.API_DB_PASSWORD || "";
+	readonly host: string = process.env.MYSQLHOST || "";
+	readonly database: string = process.env.MYSQLDATABASE || "";
+	readonly port: number = parseInt(process.env.MYSQLPORT || "0");
+	readonly user: string = process.env.MYSQLUSER || "";
+	readonly password: string = process.env.MYSQLPASSWORD || "";
 }
 
 
