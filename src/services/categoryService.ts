e3778ca5ab55598490af5c4e44b5635d8c32c247
kefer <morafi.1999.16@gmail.com>
2022-12-14 10:28:20 -0500

    se elimina archivos que no se utilizan

--- src/services/categoryService.ts
+++
@@ -1,7 +0,0 @@
-// import { getAllCategories_sql } from "../databases/sql/category";
-
-// export const getAllCategories_services = async () => {
-// 	const allCategories: any = await getAllCategories_sql();
-// 	return allCategories;
-// };
-
