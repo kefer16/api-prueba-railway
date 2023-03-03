cb289cc88e62e3583e61b92703ed3891e0884a6f
kefer <morafi.1999.16@gmail.com>
2023-03-03 11:37:06 -0500

    se agrega archivo vercel

--- src/models/server.ts
+++ src/models/server.ts
@@ -11,7 +11,7 @@ export class Server {
 	};
 	constructor() {
 		this.app = express();
-		this.port = process.env.API_PORT || "3000";
+		this.port = process.env.API_PORT || "8090";
 		this.dbConnection();
 		this.middlewares();
 		//definir mis rutas
